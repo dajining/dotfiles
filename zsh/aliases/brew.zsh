b6b0aad9e5761f9f3dda9dde10dfccb9c777fbea
小田島 悠 <odajimayu@odajima-no-MacBook-Pro.local>
2017-04-13 21:25:02 +0900

    add zplug

---
+++ zsh/aliases/brew.zsh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+alias brewall='brew update  && brew upgrade && brew cleanup && brew prune && brew doctor'
+alias brewdump='brew bundle dump -f'
---
+++ zsh/aliases/rails.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+alias rs='rails s'
+alias rc='rails c'
+alias be='bundle exec'
